712c3761e0c8fabe70c8b34d7dd202a12a7289f2
Navindu Amarakoon <navanavi3@gmail.com>
2023-04-18 13:03:07 +0530

    feat: login page imprevements

--- components/auth/LoginPage.tsx
+++ components/auth/LoginPage.tsx
@@ -1,18 +1,29 @@
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { TiWaves } from "react-icons/ti";
 
 type Props = {};
 
 function LoginPage({}: Props) {
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col gap-16">
-      <Image src="/spotify.png" alt="spotify logo" width={236} height={71} />
+      <div className="text-5xl flex items-center">
+        Playwave
+        <TiWaves className="pt-2" />
+      </div>
       <button
         onClick={() => signIn("spotify")}
         className="w-full max-w-xs rounded-full py-3 bg-[#1db954] font-bold flex items-center justify-center"
       >
         Log in with Spotify
       </button>
+
+      <Image
+        src="/spotify.png"
+        alt="spotify logo"
+        width={236 / 2}
+        height={71 / 2}
+      />
     </div>
   );
 }
