0c2dbe88310e925a1ee23661d898e09b619cc9c1
Navindu Amarakoon <navanavi3@gmail.com>
2023-04-25 20:06:12 +0530

    feat: google analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Script from "next/script";
 
 type Props = {
   children: ReactNode;
@@ -10,6 +11,20 @@ function Layout({ children, className = "" }: Props) {
     <main
       className={`w-full min-h-screen bg-black text-white antialiased ${className}`}
     >
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PYJL53E8H2"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-PYJL53E8H2');
+        `}
+      </Script>
+
       {children}
     </main>
   );
